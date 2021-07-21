f619670fbb55ac5d7a0e3ff544c691c35ef31634
ldlywt <aisier925329>
2021-07-21 21:39:16 +0800

    [N/A]WuTao:代码优化

--- app/src/main/java/com/aisier/net/ApiService.kt
+++ app/src/main/java/com/aisier/net/ApiService.kt
@@ -1,17 +1,10 @@
 package com.aisier.net
 
-import com.aisier.architecture.entity.ApiResponse
 import com.aisier.architecture.entity.BaseResponse
 import com.aisier.bean.WxArticleBean
 import retrofit2.http.GET
 
 interface ApiService {
-
-    @GET("wxarticle/chapters/json")
-    suspend fun getWxArticle(): ApiResponse<List<WxArticleBean>>
-
-    @GET("abc/chapters/json")
-    suspend fun getWxArticleError(): ApiResponse<List<WxArticleBean>>
 
     @GET("wxarticle/chapters/json")
     suspend fun getWxArticleV2(): BaseResponse<List<WxArticleBean>>
